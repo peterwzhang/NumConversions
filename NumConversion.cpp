52c3ae80fd028a642c7305e8c944ab8aff79e8c2
pwzhang21 <pwzhang@crimson.ua.edu>
2021-08-20 08:24:59 -0500

    update files

--- NumConversion.cpp
+++ NumConversion.cpp
@@ -62,13 +62,13 @@ int getInt(std::string_view prompt){
     return userInput;
 }
 
-int generateRandomInt(int min, int max){
+int generateRandomInt(const int min, const int max){
     static std::mt19937 mt(static_cast<std::mt19937::result_type>(std::time(nullptr)));
     std::uniform_int_distribution rand(min, max);
     return rand(mt);
 }
 template <typename T>
-void printProblem(T number, NumberSystem convertFrom, NumberSystem convertTo){
+void printProblem(const T number, const NumberSystem convertFrom, const NumberSystem convertTo){
     std::cout << "Convert ";
     switch (convertFrom){
         case NumberSystem::octal:
@@ -88,8 +88,46 @@ void printProblem(T number, NumberSystem convertFrom, NumberSystem convertTo){
     std::cout << number << " to " << nsToString(convertTo) << ": \n";
 }
 
-void generateProblem(int randNum){
-    int problemNumber { (generateRandomInt(0, std::numeric_limits<std::uint8_t>::max())) };
+int getAns(const NumberSystem convertTo){
+    std::cout << "Please enter an ";
+    // ? this probably is not needed, but will test
+    switch (convertTo){
+        case NumberSystem::binary:
+            std::cout << "binary number (put a 0b in the front): ";
+            std::cin >> std::dec;
+        case NumberSystem::octal:
+            std::cout << "octal number (put a 0 in the front): ";
+            std::cin >> std::oct;
+            break;
+        case NumberSystem::decimal:
+            std::cout << "decimal number: ";
+            std::cin >> std::dec;
+            break;
+        case NumberSystem::hexadecimal:
+            std::cout << "hexadecimal number (put a 0x in the front): ";
+            std::cin >> std::hex;
+            break;
+        default:
+            return -1;
+    }
+    int ans{};
+    std::cin >> ans;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return ans;
+}
+
+template <size_t S>
+bool testProblem(const std::bitset<S> problemNumber, const NumberSystem convertTo){
+    
+}
+
+bool testProblem(const int problemNumber, const NumberSystem convertTo){
+
+}
+
+bool generateProblem(int randNum){
+    const int problemNumber { (generateRandomInt(0, std::numeric_limits<std::uint8_t>::max())) }; // 0-255 range should be good
     switch(static_cast<ProblemTypes>(randNum)){
         case ProblemTypes::binaryToOctal:
             printProblem(std::bitset<8>(problemNumber), NumberSystem::binary, NumberSystem::octal);
@@ -128,12 +166,12 @@ void generateProblem(int randNum){
             printProblem(problemNumber, NumberSystem::hexadecimal, NumberSystem::decimal);
             break;
         default: // should never reach here
-            return;
+            return false;
     }
 }
 
 int main(){
-    int numbersToConvert{ getInt("How many numbers would you like to practice? ") };
+    const int numbersToConvert{ getInt("How many numbers would you like to practice? ") };
     for (auto i{0}; i < numbersToConvert; ++i){
         std::cout << std::dec << i + 1 << ": ";
         generateProblem(generateRandomInt(0, static_cast<int>(ProblemTypes::maxIndex) - 1));
